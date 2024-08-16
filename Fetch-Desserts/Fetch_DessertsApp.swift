ddb2ed3baa85b8dc50d6fe1c1657c473e3e97bfd
SophiaCG <sophia.gorgonio@gmail.com>
2024-08-16 10:39:43 -0400

    Initial commit

---
+++ Fetch-Desserts/Fetch_DessertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_DessertsApp.swift
+//  Fetch-Desserts
+//
+//  Created by Sophia Gorgonio on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_DessertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
